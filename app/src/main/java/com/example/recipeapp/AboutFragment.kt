d4edddbc237e4f40d831c08a36220a6dbcde41d0
Sendzo Matowane Mfo Matooane <ST10369554@rcconnect.edu.za>
2025-10-14 19:56:36 +0200

    About page updated

--- app/src/main/java/com/example/recipeapp/AboutFragment.kt
+++ app/src/main/java/com/example/recipeapp/AboutFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 
 class AboutFragment : Fragment() {
 
@@ -13,7 +14,27 @@ class AboutFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_about, container, false)
+        val view = inflater.inflate(R.layout.fragment_about, container, false)
+
+        val title = view.findViewById<TextView>(R.id.about_title)
+        val description = view.findViewById<TextView>(R.id.about_description)
+        val mission = view.findViewById<TextView>(R.id.about_mission)
+        val contact = view.findViewById<TextView>(R.id.about_contact)
+
+        title.text = getString(R.string.about_title)
+        description.text = getString(R.string.about_description)
+        mission.text = getString(R.string.about_mission)
+
+        // Get version name dynamically
+        val versionName = requireContext()
+            .packageManager
+            .getPackageInfo(requireContext().packageName, 0)
+            .versionName
+
+        // Use string resource with placeholder
+        contact.text = getString(R.string.about_contact, versionName)
+
+        return view
     }
 
 }
